98145a789c355dd9d5de918f52163e25f236cb20
guustavomc <gustavomccb@gmail.com>
2026-06-16 20:22:21 -0300

    added additional unit test

---
+++ src/test/java/org/pokedex/PokedexSearchApplicationTest.java
@@ -0,0 +1,13 @@
+package org.pokedex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+public class PokedexSearchApplicationTest {
+@Test
+	void contextLoads() {
+	}
+}
